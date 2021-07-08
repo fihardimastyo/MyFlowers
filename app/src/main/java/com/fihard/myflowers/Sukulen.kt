c285386d0d2ac5ad2303c19cd62b2c1a06147d88
fihardimastyo <fiharkun@gmail.com>
2021-07-08 11:33:07 +0700

    my flowers

---
+++ app/src/main/java/com/fihard/myflowers/Sukulen.kt
@@ -0,0 +1,16 @@
+package com.fihard.myflowers
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class Sukulen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
